477ed5a5da7e729174c1a140db98699709ac653f
David Van <David@DavidVan.net>
2018-01-13 17:03:50 -0800

    Upload recent changes.

--- Emotichat/functions/create_account.js
+++ Emotichat/functions/create_account.js
@@ -1,6 +1,30 @@
+const mongoose = require('mongoose');
+const userDetails = new mongoose.Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+    },
+    userName: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+});
+
 /**
+* @param {string} email The user's email
+* @param {string} userName The user's user name.
+* @param {string} password The user's password.
 * @returns {any}
 */
-module.exports = (context, callback) => {
-  callback(null, 'hello world');
+module.exports = (email, userName, password, context, callback) => {
+
+    callback(null, `Email: ${email}, Username: ${userName}, Password: ${password}`);
 };
