dcf84c53727055c247e1193f468b250977d37f7c
David Van <David@DavidVan.net>
2018-01-13 22:37:57 -0800

    Added JWT support.

--- Emotichat/functions/authenticate_account.js
+++ Emotichat/functions/authenticate_account.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 const UserModel = require('../model/User');
+const jwt = require('jsonwebtoken');
+
+const secret = process.env['JWT_SECRET'];
 
 /**
 * @param {string} userName The user's user name.
@@ -22,7 +25,17 @@ module.exports = (userName, password, context, callback) => {
             callback(null, 'Wrong username or password!');
             return;
         }
-        callback(null, 'Success!')
         // Use JWT so user stays logged in...
+        const userData = {
+            userName: user.userName,
+            email: user.email,
+        }
+        const token = {
+            token: jwt.sign({ userData }, secret, {
+                expiresIn: "1m" // Expires in 1 month
+            }),
+        }
+
+        callback(null, token, { 'Content-Type': 'application/json'});
     });
 };
