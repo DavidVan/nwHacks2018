62cdfc4369cd8645701314184db095acf96e35b6
Harold Agnote <haroldagnote38@hotmail.com>
2018-01-13 19:31:13 -0800

    Working on send function and updated package for autoincrement

--- Emotichat/functions/send.js
+++ Emotichat/functions/send.js
@@ -1,6 +1,35 @@
+const mongoose = require('mongoose');
+const messageDetails = new mongoose.Schema({
+    uid: {
+        type: Number,
+        unique: true,
+        required: true,
+    },
+    to: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+    },
+    from: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+    },
+    message: {
+        type: String,
+        unique: false,
+        required: true,
+        trim: false,
+    }
+});
+
 /**
+* @param {string} userName The user's username
+* @param {string} message The user's message
 * @returns {any}
 */
-module.exports = (context, callback) => {
-  callback(null, 'hello world');
+module.exports = (userName, message, context, callback) => {
+  callback(null, `userName: ${userName}, Message: ${message}`);
 };
